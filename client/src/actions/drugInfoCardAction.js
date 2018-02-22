85c2b1eda67e9b32880720c7e0d28bf28d624198
Kellye Greene <kellyecodes@gmail.com>
2018-02-22 08:32:50 -0500

    WIP

--- client/src/actions/drugInfoCardAction.js
+++ client/src/actions/drugInfoCardAction.js
@@ -1,8 +1,9 @@
-var rp = require('request-promise');
+import axios from 'axios';
 
 function getDrugInfoCards() {
     const url = 'http://localhost:3001/api/DrugInfoCards'
     const request = axios.get(url)
+    debugger
   return {
     type:'GET_DRUG_INFO_CARDS',
     payload: request
